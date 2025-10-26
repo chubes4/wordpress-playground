3b79589b8aa5f9dfb89c79e5efb29076001ff115
Fellyph Cintra <fellyph.cintra@gmail.com>
2025-10-26 12:42:59 +0000

    Adding Localization for offline notice

--- packages/playground/website/src/components/offline-notice/index.tsx
+++ packages/playground/website/src/components/offline-notice/index.tsx
@@ -1,14 +1,16 @@
 import { Notice } from '@wordpress/components';
 import css from './style.module.css';
+import { useI18n } from '../../lib/i18n';
 
 export function OfflineNotice() {
+	const { __ } = useI18n();
 	return (
 		<Notice
 			status="warning"
 			isDismissible={false}
 			className={css.offlineNotice}
 		>
-			Some features may not available because you are offline.
+			{__('Some features may not available because you are offline.')}
 		</Notice>
 	);
 }
