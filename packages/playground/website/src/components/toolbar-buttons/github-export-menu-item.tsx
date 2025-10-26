55bab3e2af1bf31fbae59bf436588a0109131388
Fellyph Cintra <fellyph.cintra@gmail.com>
2025-10-26 13:01:43 +0000

    adding localization for impor and export github

--- packages/playground/website/src/components/toolbar-buttons/github-export-menu-item.tsx
+++ packages/playground/website/src/components/toolbar-buttons/github-export-menu-item.tsx
@@ -3,23 +3,27 @@ import { setActiveModal } from '../../lib/state/redux/slice-ui';
 import type { PlaygroundDispatch } from '../../lib/state/redux/store';
 import { useDispatch } from 'react-redux';
 import { modalSlugs } from '../layout';
+import { useI18n } from '../../lib/i18n';
 
 interface Props {
 	onClose: () => void;
 	disabled?: boolean;
 }
 export function GithubExportMenuItem({ onClose, disabled }: Props) {
+	const { __ } = useI18n();
 	const dispatch: PlaygroundDispatch = useDispatch();
 	return (
 		<MenuItem
-			aria-label="Export WordPress theme, plugin, or wp-content directory to a GitHub repository as a Pull Request."
+			aria-label={__(
+				'Export WordPress theme, plugin, or wp-content directory to a GitHub repository as a Pull Request.'
+			)}
 			disabled={disabled}
 			onClick={() => {
 				dispatch(setActiveModal(modalSlugs.GITHUB_EXPORT));
 				onClose();
 			}}
 		>
-			Export to GitHub
+			{__('Export to GitHub')}
 		</MenuItem>
 	);
 }
--- packages/playground/website/src/components/toolbar-buttons/github-import-menu-item.tsx
+++ packages/playground/website/src/components/toolbar-buttons/github-import-menu-item.tsx
@@ -3,23 +3,27 @@ import { setActiveModal } from '../../lib/state/redux/slice-ui';
 import type { PlaygroundDispatch } from '../../lib/state/redux/store';
 import { useDispatch } from 'react-redux';
 import { modalSlugs } from '../layout';
+import { useI18n } from '../../lib/i18n';
 
 interface Props {
 	onClose: () => void;
 	disabled?: boolean;
 }
 export function GithubImportMenuItem({ onClose, disabled }: Props) {
+	const { __ } = useI18n();
 	const dispatch: PlaygroundDispatch = useDispatch();
 	return (
 		<MenuItem
-			aria-label="Import WordPress theme, plugin, or wp-content directory from a GitHub repository."
+			aria-label={__(
+				'Import WordPress theme, plugin, or wp-content directory from a GitHub repository.'
+			)}
 			disabled={disabled}
 			onClick={() => {
 				dispatch(setActiveModal(modalSlugs.GITHUB_IMPORT));
 				onClose();
 			}}
 		>
-			GitHub repository
+			{__('GitHub repository')}
 		</MenuItem>
 	);
 }
