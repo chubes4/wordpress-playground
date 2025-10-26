3080a0d75b43add22b4af984f0567c92934751c1
Fellyph Cintra <fellyph.cintra@gmail.com>
2025-10-26 12:39:27 +0000

    adding localization to PR menu

--- packages/playground/website/src/components/toolbar-buttons/wordpress-pr-menu-item.tsx
+++ packages/playground/website/src/components/toolbar-buttons/wordpress-pr-menu-item.tsx
@@ -3,23 +3,25 @@ import { setActiveModal } from '../../lib/state/redux/slice-ui';
 import type { PlaygroundDispatch } from '../../lib/state/redux/store';
 import { useDispatch } from 'react-redux';
 import { modalSlugs } from '../layout';
+import { useI18n } from '../../lib/i18n';
 
 interface Props {
 	onClose: () => void;
 	disabled?: boolean;
 }
 export function WordPressPRMenuItem({ onClose, disabled }: Props) {
+	const { __ } = useI18n();
 	const dispatch: PlaygroundDispatch = useDispatch();
 	return (
 		<MenuItem
-			aria-label="Preview WordPress Core PR"
+			aria-label={__('Preview WordPress Core PR')}
 			disabled={disabled}
 			onClick={() => {
 				dispatch(setActiveModal(modalSlugs.PREVIEW_PR_WP));
 				onClose();
 			}}
 		>
-			WordPress Core PR
+			{__('WordPress Core PR')}
 		</MenuItem>
 	);
 }
