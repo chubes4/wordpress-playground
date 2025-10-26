5fd8a528c6c1ba366e82126e34593efa05d63de6
Fellyph Cintra <fellyph.cintra@gmail.com>
2025-10-26 12:39:46 +0000

    Adding localization for view logs

--- packages/playground/website/src/components/toolbar-buttons/view-logs.tsx
+++ packages/playground/website/src/components/toolbar-buttons/view-logs.tsx
@@ -5,22 +5,24 @@ import { useDispatch } from 'react-redux';
 import type { PlaygroundDispatch } from '../../lib/state/redux/store';
 import { setActiveModal } from '../../lib/state/redux/slice-ui';
 import { modalSlugs } from '../layout';
+import { useI18n } from '../../lib/i18n';
 
 type Props = { onClose: () => void };
 export function ViewLogs({ onClose }: Props) {
+	const { __ } = useI18n();
 	const dispatch: PlaygroundDispatch = useDispatch();
 	return (
 		<MenuItem
 			icon={details}
 			iconPosition="left"
 			data-cy="view-logs"
-			aria-label="View logs"
+			aria-label={__('View logs')}
 			onClick={() => {
 				dispatch(setActiveModal(modalSlugs.LOG));
 				onClose();
 			}}
 		>
-			View logs
+			{__('View logs')}
 		</MenuItem>
 	);
 }
