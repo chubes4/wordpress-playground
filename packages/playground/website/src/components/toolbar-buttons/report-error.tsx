deb803d5ff562acf612a405a2c747f4cb3e07e61
Fellyph Cintra <fellyph.cintra@gmail.com>
2025-10-26 12:40:16 +0000

    adding localization for view error component

--- packages/playground/website/src/components/toolbar-buttons/report-error.tsx
+++ packages/playground/website/src/components/toolbar-buttons/report-error.tsx
@@ -4,21 +4,23 @@ import { useDispatch } from 'react-redux';
 import type { PlaygroundDispatch } from '../../lib/state/redux/store';
 import { setActiveModal } from '../../lib/state/redux/slice-ui';
 import { modalSlugs } from '../layout';
+import { useI18n } from '../../lib/i18n';
 
 type Props = { onClose: () => void; disabled?: boolean };
 export function ReportError({ onClose, disabled }: Props) {
+	const { __ } = useI18n();
 	const dispatch: PlaygroundDispatch = useDispatch();
 	return (
 		<MenuItem
 			data-cy="report-error"
-			aria-label="Report an error in Playground"
+			aria-label={__('Report an error in Playground')}
 			disabled={disabled}
 			onClick={() => {
 				dispatch(setActiveModal(modalSlugs.ERROR_REPORT));
 				onClose();
 			}}
 		>
-			Report error
+			{__('Report error')}
 		</MenuItem>
 	);
 }
